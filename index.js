242a14d8b8453e79395e4a41bb5d884fde3efc1b
Maja Dahlberg <maja04dahlberg@gmail.com>
2024-12-18 14:04:18 +0100

    Remove - klart
    Gjort klart remove-sidan

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const numbers = document.querySelector("#numbers")
+const numbers = document.querySelector("#numbers");
 
 function randomNumber (min, max) { 
     return Math.floor(min + (max - min) * Math.random()); // denna funktion väljer en random siffra mellan det minsta och högst tal man skriver när man anropar funktionen
@@ -7,14 +7,15 @@ function randomNumber (min, max) {
 
 
 function newNumbers (n, fill) {
-    let numbersContainer = document.getElementById("numbers")
+    let numbersContainer = document.getElementById("numbers");
     numbersContainer.innerHTML = ""; // för varje gång man trycker på knappen "create" tömsinnehållet 
     
-
+    
 
     for (let i = 0; i < n; i++) {
-        const div = document.createElement("div"); 
+        const div = document.createElement("div"); // div är namnet för små-divvarna, använd till background-color grejen
         numbersContainer.appendChild(div);  
+        div.className = "gridNumber";
 
         div.style.backgroundColor = "rgb(117, 174, 183)";
         div.style.borderRadius = "4px";
@@ -32,10 +33,32 @@ function newNumbers (n, fill) {
         }
 
     }
+
+
 }
 
 document.getElementById("button_id").addEventListener("click", function (){ // gör så att när man klickar på "create"-knappen så kommer det man skriver i input att påverka hur många siffror som dyker upp på sidan
     let n = document.getElementById("input_same_on_all_pages").value;
     newNumbers(n, true);
 })
+
+
+
+// let newColorWhenHoverNumbers = document.getElementById("div"); 
+// let originalColorOfNumbers = document.getElementById("div");
+
+// function hoverEffect () {
+//     newColorWhenHoverNumbers.addEventListener("mouseover", function(){
+//         newColorWhenHoverNumbers.style.backgroundColor = "rgb(63, 83, 119)";
+//     })
+
+//     originalColorOfNumbers.addEventListener("mouseover", function(){
+//         originalColorOfNumbers.style.backgroundColor = "blue";
+//     })
+
+// }
+ 
+
+
+
 
